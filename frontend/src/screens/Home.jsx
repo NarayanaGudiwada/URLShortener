e8cc1e404354d77b24a89e3f9fa19bd4025df370
NarayanaGudiwada <95913215+NarayanaGudiwada@users.noreply.github.com>
2024-12-22 14:41:18 +0530

    Revert "Revert "home page changes""
    This reverts commit ae02f1b621b656994a440cc75bed903e06b5a349.

--- frontend/src/screens/Home.jsx
+++ frontend/src/screens/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
       <Card>
         <div className="flex flex-col items-center">
           <h1 className="text-xl font-bold text-orange-400 ">
-            Enter <span className="text-orange-950">URL</span> to continue
+            Enter <span className="text-orange-950">URL</span> to shorten
           </h1>
           <form action={handleSubmit}>
             <input
