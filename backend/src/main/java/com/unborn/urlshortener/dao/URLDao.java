4294481cf6a06ed1d0be61d752529ec9f8634fe0
subbu192 <manchalasubramanyam2@gmail.com>
2024-12-22 15:23:34 +0530

    Implemented URLDao and URLDaoWrapper

---
+++ backend/src/main/java/com/unborn/urlshortener/dao/URLDao.java
@@ -0,0 +1,23 @@
+package com.unborn.urlshortener.dao;
+
+import com.unborn.urlshortener.entity.URLEntity;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.RepositoryDefinition;
+
+import java.util.List;
+
+@RepositoryDefinition(domainClass = URLEntity.class, idClass = Long.class)
+@Transactional
+public interface URLDao {
+
+    public URLEntity save(URLEntity longUrlEntity);
+
+    @Modifying
+    @Query("UPDATE URLEntity url SET url.noOfClicks = url.noOfClicks + 1 WHERE url.id = :id")
+    public void incrementClickCount(Long id);
+
+    public URLEntity findById(Long id);
+    public List<URLEntity> findAll();
+}
