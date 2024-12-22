4d158b100f3ef354cd6fa500cf5fe950bb078672
NarayanaGudiwada <95913215+NarayanaGudiwada@users.noreply.github.com>
2024-12-22 14:40:36 +0530

    Revert "front end basic feat"
    This reverts commit 0b8c2c94b68cf703416ac0fbb8caad65f53909ec.

--- backend/src/main/java/com/unborn/urlshortener/controller/URLController.java
+++ backend/src/main/java/com/unborn/urlshortener/controller/URLController.java
@@ -4,17 +4,17 @@ import com.unborn.urlshortener.dto.ShortURLRequest;
 import com.unborn.urlshortener.service.URLService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin
-@RequestMapping("/api/v1")
 public class URLController {
 
     @Autowired
     URLService urlService;
 
-    @PostMapping("/shorten")
+    @PostMapping("/shortenURL")
     public ResponseEntity<String> shortenURL(@RequestBody ShortURLRequest shortURLRequest) {
         return urlService.shortenURL(shortURLRequest.getURL());
     }
--- frontend/src/screens/Home.jsx
+++ frontend/src/screens/Home.jsx
@@ -1,41 +1,26 @@
-import { useRef, useState } from "react";
-import { SERVICE_API, SERVICE_URL } from "../../config/apiConfig";
 import Card from "../components/Card";
-import axios from "axios";
 
 const Home = () => {
-  const link = useRef();
-  const [shortLink, setLink] = useState("");
-  const handleSubmit = async (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    const url = link.current.value;
-    if (!url) return;
-    try {
-      const response = await axios.post(SERVICE_API + "shorten", { url });
-      setLink(response.data);
-    } catch (error) {
-      console.log(error);
-    }
-    link.current.value = "";
-  };
-
-  const handleCopy = () => {
-    navigator.clipboard.writeText(SERVICE_URL + shortLink);
-    alert("URL copied to clipboard!");
+    const url = e.target[0].value;
+    console.log(url);
   };
 
   return (
-    <div className="flex flex-grow justify-center items-center bg-orange-50 h-dvh">
+    <div
+      className=" flex flex-grow justify-center items-center bg-orange-50 h-dvh
+    "
+    >
       <Card>
         <div className="flex flex-col items-center">
-          <h1 className="text-xl font-bold text-orange-400">
+          <h1 className="text-xl font-bold text-orange-400 ">
             Enter <span className="text-orange-950">URL</span> to shorten
           </h1>
           <form onSubmit={handleSubmit}>
             <input
               type="text"
-              ref={link}
-              className="w-72 h-10 rounded-lg p-2 mt-4 focus:border border-orange-400 focus:outline-none"
+              className="w-72 h-10 rounded-lg p-2 mt-4 flex justify-center focus:border-orange-400"
               placeholder="Enter URL"
             />
             <button
@@ -45,24 +30,6 @@ const Home = () => {
               Shorten URL
             </button>
           </form>
-
-          {shortLink && (
-            <div className="mt-4 flex items-center">
-              <a
-                href={SERVICE_URL + shortLink}
-                target="_blank"
-                className="mr-2"
-              >
-                {SERVICE_URL + shortLink}
-              </a>
-              <button
-                onClick={handleCopy}
-                className="bg-orange-400 text-white rounded-lg px-2 py-1"
-              >
-                Copy
-              </button>
-            </div>
-          )}
         </div>
       </Card>
     </div>
