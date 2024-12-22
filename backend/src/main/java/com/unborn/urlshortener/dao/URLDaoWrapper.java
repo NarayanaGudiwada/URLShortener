91601b5203b5802e52a92964d4fd2ba11b04f338
subbu192 <manchalasubramanyam2@gmail.com>
2024-12-22 15:23:34 +0530

    Implemented URLDao and URLDaoWrapper

---
+++ backend/src/main/java/com/unborn/urlshortener/dao/URLDaoWrapper.java
@@ -0,0 +1,46 @@
+package com.unborn.urlshortener.dao;
+
+import com.unborn.urlshortener.dto.URLBean;
+import com.unborn.urlshortener.entity.URLEntity;
+import com.unborn.urlshortener.utils.BEConverter;
+import com.unborn.urlshortener.utils.URLShortener;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Repository
+@Transactional
+public class URLDaoWrapper {
+    @Autowired
+    private URLDao urlDao;
+
+    public URLBean getShortUrl(URLBean longUrlBean) {
+        URLEntity longUrlEntity = BEConverter.convertBeanToEntity(longUrlBean);
+
+        Long urlId = new Date().getTime();
+        String shortUrl = URLShortener.convertIdToShortUrl(urlId);
+        longUrlEntity.setId(urlId);
+        longUrlEntity.setShortUrl(shortUrl);
+
+        URLEntity shortUrlEntity = urlDao.save(longUrlEntity);
+        return BEConverter.convertEntityToBean(shortUrlEntity);
+    }
+    public URLBean getLongUrl(String shortUrl) {
+        Long urlId = URLShortener.convertShortUrlToId(shortUrl);
+        urlDao.incrementClickCount(urlId);
+        URLEntity longUrlEntity = urlDao.findById(urlId);
+        return BEConverter.convertEntityToBean(longUrlEntity);
+    }
+    public List<URLBean> getAllUrls() {
+        List<URLEntity> urlEntityList = urlDao.findAll();
+        List<URLBean> urlBeanList = new ArrayList<>();
+        for (URLEntity urlEntity : urlEntityList) {
+            urlBeanList.add(BEConverter.convertEntityToBean(urlEntity));
+        }
+        return urlBeanList;
+    }
+}
