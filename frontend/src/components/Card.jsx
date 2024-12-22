bf4a39268084f0aa0cb833e3c7bf91b2d9b8e584
NarayanaGudiwada <95913215+NarayanaGudiwada@users.noreply.github.com>
2024-12-22 14:40:36 +0530

    Revert "changing colors, adding card component, home page modification"
    This reverts commit c7a8d3732baed271e059f876f0a61b53c94c820f.

--- frontend/src/components/Card.jsx
+++
@@ -1,9 +0,0 @@
-const Card = ({ children }) => {
-  return (
-    <div className="size-96 h-60 bg-orange-200 rounded-lg shadow-lg p-8">
-      {children}
-    </div>
-  );
-};
-
-export default Card;
