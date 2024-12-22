4134afc06f83a670a1cb4400b3ca0df1eb2f3b37
subbu192 <manchalasubramanyam2@gmail.com>
2024-12-22 15:24:06 +0530

    Implemented URLService and URLServiceImpl

---
+++ backend/src/main/java/com/unborn/urlshortener/service/URLServiceImpl.java
@@ -0,0 +1,29 @@
+package com.unborn.urlshortener.service;
+
+import com.unborn.urlshortener.dao.URLDaoWrapper;
+import com.unborn.urlshortener.dto.URLBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class URLServiceImpl implements URLService {
+    @Autowired
+    private URLDaoWrapper urlDaoWrapper;
+
+    @Override
+    public URLBean getShortUrl(URLBean longUrlBean) {
+        return urlDaoWrapper.getShortUrl(longUrlBean);
+    }
+
+    @Override
+    public URLBean getLongUrl(String shortUrl) {
+        return urlDaoWrapper.getLongUrl(shortUrl);
+    }
+
+    @Override
+    public List<URLBean> getAllUrls() {
+        return urlDaoWrapper.getAllUrls();
+    }
+}
