8d154adfa2653a13f6e8038aef344da608cc89da
NarayanaGudiwada <95913215+NarayanaGudiwada@users.noreply.github.com>
2024-12-22 14:41:18 +0530

    Revert "Revert "changing colors, adding card component, home page modification""
    This reverts commit e2d910de831e8e570f2eb7376f302c040e376e15.

--- frontend/src/screens/Home.jsx
+++ frontend/src/screens/Home.jsx
@@ -1,5 +1,39 @@
+import Card from "../components/Card";
+
 const Home = () => {
-  return <div className="flex justify-center">URL shortner</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const url = e.target[0].value;
+    console.log(url);
+  };
+
+  return (
+    <div
+      className=" flex flex-grow justify-center items-center bg-orange-50 h-dvh
+    "
+    >
+      <Card>
+        <div className="flex flex-col items-center">
+          <h1 className="text-xl font-bold text-orange-400 ">
+            Enter <span className="text-orange-950">URL</span> to continue
+          </h1>
+          <form action={handleSubmit}>
+            <input
+              type="text"
+              className="w-72 h-10 rounded-lg p-2 mt-4 flex justify-center focus:border-orange-400"
+              placeholder="Enter URL"
+            />
+            <button
+              type="submit"
+              className="w-72 h-10 bg-orange-400 text-white rounded-lg mt-4"
+            >
+              Shorten URL
+            </button>
+          </form>
+        </div>
+      </Card>
+    </div>
+  );
 };
 
 export default Home;
